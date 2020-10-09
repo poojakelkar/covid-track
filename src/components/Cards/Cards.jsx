9124443222c6ac7adbe889201f2b733cb6d5dfe8
Pooja Kelkar <poojakelkar53@gmail.com>
2020-10-09 20:33:37 +0530

    added chart covid cases in cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,6 +8,7 @@ import {
 } from "@material-ui/core";
 import CountUp from "react-countup";
 import styles from "./Cards.module.css";
+import cx from "classnames";
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     if (!confirmed) {
@@ -17,7 +18,12 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>
-                <Grid item component={Card}>
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Infected
@@ -30,30 +36,58 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                                 separator=','
                             />
                         </Typography>
-                        <Typography color='textSecondary'>REAL DATE</Typography>
+                        <Typography color='textSecondary'>
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
                         <Typography variant='body2'>
                             Number of active cases of COVID-19
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Recovered
                         </Typography>
-                        <Typography variant='h5'>REAL DATA</Typography>
-                        <Typography color='textSecondary'>REAL DATE</Typography>
+                        <Typography variant='h5'>
+                            <CountUp
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography color='textSecondary'>
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
                         <Typography variant='body2'>
                             Number of recoveries from COVID-19
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Deaths
                         </Typography>
-                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography variant='h5'>
+                            <CountUp
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=','
+                            />
+                        </Typography>
                         <Typography color='textSecondary'>REAL DATE</Typography>
                         <Typography variant='body2'>
                             Number of deaths caused by COVID-19
