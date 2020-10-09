9c4b8737c198dc554b4ef2619c6db25d8c9b4f64
Pooja Kelkar <poojakelkar53@gmail.com>
2020-10-09 13:18:07 +0530

    creating covid-tracker

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+    state = {
+        data: {},
+    };
+
+    async componentDidMount() {
+        const fetchData = await fetchData();
+
+        this.setState({ data: fetchData });
+    }
+
+    render() {
+        const { data } = this.state;
+
+        return (
+            <div className={styles.container}>
+                <Cards data={data} />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
+}
+
+export default App;
