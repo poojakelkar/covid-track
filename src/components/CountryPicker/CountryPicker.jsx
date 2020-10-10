46ad0e946cd84c106fbe24511c8d88f71915ad0b
Pooja Kelkar <poojakelkar53@gmail.com>
2020-10-10 18:50:52 +0530

    done changes and complete project

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -25,7 +25,7 @@ const CountryPicker = ({ handleCountryChange }) => {
             <NativeSelect
                 defaultValue=''
                 onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value="global">Global</option>
+                <option value="">Global</option>
                 {fetchedCountries.map((country, i) => 
                     <option key={i} value={country}>
                         {country}
